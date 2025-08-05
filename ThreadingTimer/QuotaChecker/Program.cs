733f9320144331c7410b3d2f15abc7011e3f3038
Zenyno <jonathan.klcflam@gmail.com>
2025-08-05 17:26:33 +1000

    JCTM-2 Add new System.Threading.Timer solution and new QuotaChecker console project

---
+++ ThreadingTimer/QuotaChecker/Program.cs
@@ -0,0 +1,18 @@
+﻿using QuotaChecker;
+
+var scale = Scale.GetScale();
+scale.QuotaLimit = 2;
+while (true)
+{
+    if (scale.IsQuotaReached)
+    {
+        Console.WriteLine($"Processed [{scale.Unit}] units. Quota has been reached!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"Processed [{scale.Unit}] units. Quota is still available.");
+    }
+
+    Thread.Sleep(2000);
+}
